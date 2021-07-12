2beeecebbacef0c155a3614183fd87e227da258c
moraguma <g170603@dac.unicamp.br>
2021-07-11 21:50:58 -0300

    JogadorHumano retornado

--- NomeEquipe_Projeto/src/com/unicamp/mc322/projeto/jogo/Jogo.java
+++ NomeEquipe_Projeto/src/com/unicamp/mc322/projeto/jogo/Jogo.java
@@ -24,7 +24,7 @@ public class Jogo {
 	}
 	
 	private void start() {
-		Jogador jogador1 = new Bot(20, new DeckDemaciaFactory());
+		Jogador jogador1 = new JogadorHumano(20, new DeckDemaciaFactory());
 		Jogador jogador2 = new Bot(20, new DeckDemaciaFactory());
 		this.mesa = new Mesa(jogador1, jogador2);
 		
